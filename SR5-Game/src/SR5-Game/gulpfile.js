493f0155fc90c4eff76bf86cea894f22547f431a
Jean-Michel Deruty <jmderuty@stormancer.com>
2016-03-06 17:31:21 +0100

    Initial commit

---
+++ SR5-Game/src/SR5-Game/gulpfile.js
@@ -0,0 +1,16 @@
+﻿var paths = {
+  webroot: "./wwwroot/",
+  node_modules: "./node_modules/"
+};
+
+var gulp = require("gulp"),
+    fs = require("fs"),
+    less = require("gulp-less");
+
+var project = require('./project.json');
+
+gulp.task("less", function () {
+  return gulp.src('styles/site.less')
+          .pipe(less())
+          .pipe(gulp.dest(paths.webroot + '/css'));
+});
