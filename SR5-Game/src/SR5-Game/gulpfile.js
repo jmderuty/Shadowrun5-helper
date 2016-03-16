d747724e072a4145325ca091d2d616462a5eb393
Julien <julien@Eole.local>
2016-03-16 22:59:32 +0100

    [wip] New mockup

--- SR5-Game/src/SR5-Game/gulpfile.js
+++ SR5-Game/src/SR5-Game/gulpfile.js
@@ -1,16 +1,33 @@
 ﻿var paths = {
   webroot: "./wwwroot/",
-  node_modules: "./node_modules/"
+  node_modules: "./node_modules/",
+  less_glob: "./styles/*.less"
 };
 
 var gulp = require("gulp"),
     fs = require("fs"),
     less = require("gulp-less");
 
+var browserSync = require('browser-sync').create();
 var project = require('./project.json');
 
+
 gulp.task("less", function () {
-  return gulp.src('styles/site.less')
+  return gulp.src(paths.less_glob)
           .pipe(less())
-          .pipe(gulp.dest(paths.webroot + '/css'));
+          .pipe(gulp.dest(paths.webroot + '/css'))
+          .pipe(browserSync.stream());
 });
+
+
+gulp.task('serve', ['less'], function() {
+
+    browserSync.init({
+        server: paths.webroot,
+    });
+
+    gulp.watch(paths.less_glob, ['less']);
+    gulp.watch(paths.webroot+"*.html").on('change', browserSync.reload);
+});
+
+gulp.task('default', ['serve']);
